b6bf49dd333752ebf3790484a139905602bf1802
简的mac <39438074+JianJia2018@users.noreply.github.com>
2026-04-18 13:19:10 +0800

    build: 精简 luci feeds 替换，升级 CMake 至最新版
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -28,6 +28,5 @@ fi
 
 sudo sed -i 's:/bin/ash:/bin/bash:g' /etc/passwd
 
-sed -i '/src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23.05/s/^/#/' "feeds.conf.default"
-echo "src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-24.10" >> "feeds.conf.default"
+sed -i 's|;openwrt-23.05|;openwrt-24.10|' "feeds.conf.default"
 ./scripts/feeds update -a && ./scripts/feeds install -a
